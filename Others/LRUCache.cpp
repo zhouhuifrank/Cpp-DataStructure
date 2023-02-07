87e864758b4e3f67a6a2d2421e41347900bf422d
frankzhou <2280610818@qq.com>
2023-02-08 00:30:22 +0800

    LRU缓存淘汰

--- Others/LRUCache.cpp
+++ Others/LRUCache.cpp
@@ -1,3 +1,96 @@
-//
-// Created by 22806 on 2023/2/4.
-//
+/*
+ *  this.FrankZhou
+ *  节点类实现类
+ *  双向链表实现类
+ *  LRUCaches实现类
+ */
+
+#include "LRUCache.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <unordered_map>
+
+// Node节点实现类
+Node::Node(int key, int value, Node *prev, Node *next) {
+    this->key = key;
+    this->value = value;
+    this->prev = prev;
+    this->next = next;
+}
+
+Node::Node() {
+    this->prev = nullptr;
+    this->next = nullptr;
+}
+
+Node::~Node() {
+}
+
+
+// DoubleLinkList双向链表实现类
+Node* DoubleLinkList::move(int i) const {
+
+}
+
+DoubleLinkList::DoubleLinkList() {
+
+}
+
+DoubleLinkList::~DoubleLinkList() {
+
+}
+
+bool DoubleLinkList::isEmpty() const {
+
+}
+
+int DoubleLinkList::length() const {
+
+}
+
+void DoubleLinkList::addFromLast(int key, int value) {
+
+}
+
+void DoubleLinkList::removeFromFirst() {
+
+}
+
+void DoubleLinkList::remove() {
+
+}
+
+// LRUCache实现类
+void LRUCache::makeRecently(int key) {
+
+}
+
+void LRUCache::addRecently(int key, int value) {
+
+}
+
+void LRUCache::deleteKey(int key) {
+
+}
+
+void LRUCache::removeLeastRecently() {
+
+}
+
+LRUCache::LRUCache(int capacity) {
+
+}
+
+LRUCache::~LRUCache() noexcept {
+
+}
+
+int LRUCache::get(int key) const {
+
+}
+
+void LRUCache::put(int key, int value) {
+
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    typedef pair<int,int> pii;
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> answer;
+
+        // pair的first存放下标,second存放温度
+        stack<pii> stk;
+        for (int i=temperatures.size()-1;i>=0;i--) {
+            int target = temperatures[i];
+
+            while (!stk.empty()) {
+                if (stk.top().second <= target) {
+                    stk.pop();
+                } else {
+                    break;
+                }
+            }
+
+            if (!stk.empty()) {
+                answer[i] = stk.top().first - i;
+            } else {
+                answer[i] = 0;
+            }
+
+            stk.push(make_pair(i,target));
+        }
+
+        return answer;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    unordered_map<int,int> mp;
+    mp[1] = 99;
+    cout << mp.find(1)->second << endl;
+    auto it = mp.find(2);
+    if (it == nullptr) {
+        cout << "null poniter" << endl;
+    }
+
     return 0;
 }
